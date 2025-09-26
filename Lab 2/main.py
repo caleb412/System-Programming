314b4431c1ddbb7001e606554bb07325d5ba5863
Caleb Chanda <157166920+caleb412@users.noreply.github.com>
2025-09-26 13:06:05 +0300

    Changed main with another function

--- Lab 2/main.py
+++ Lab 2/main.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import find_dotenv, load_dotenv
-
+from pathlib import Path
 load_dotenv(find_dotenv())
-path = os.getenv("PATH")
+PATH = os.getenv("PATH")
 
 def count_directory_size(directory):
 
@@ -12,9 +12,20 @@ def count_directory_size(directory):
         count +=1
     print(count)
 
+#Высокоуровневый подход
+def count_folder_size(directory):
+    path = Path(directory)
+    folder_size = 0
+    for x in path.iterdir():
+        folder_size+=1
+    return folder_size
+
+
 if __name__ == '__main__':
-    filepath = input("Enter a file path to access:")
-    if os.path.exists(filepath):
-        count_directory_size(filepath)
-    else:
-        count_directory_size(path)
+    print(count_folder_size('C:/Users'))
+    # filepath = input("Enter a file path to access:")
+    # if os.path.exists(filepath):
+    #     count_directory_size(filepath)
+    # else:
+    #     # count_directory_size(PATH)
+    #
